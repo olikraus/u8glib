0bdf994e7ccac1e1bda28b942062ee1a8ff66ea9
olikraus <devnull@localhost>
2011-12-28 01:42:17 +0100

    cpp interface

--- sys/sdl/cpp_helloworld/main.cpp
+++ sys/sdl/cpp_helloworld/main.cpp
@@ -11,16 +11,12 @@ U8GLIB u8g(&u8g_dev_sdl_1bit);
 
 
 int main(void)
-{
-  
-  //u8g_t u8g;
-  //u8g_Init(&u8g, &u8g_dev_sdl_1bit);
+{  
   u8g.firstPage();
   
   do
   {
     u8g.setFont(u8g_font_unifont);
-    
     u8g.drawStr(0, 20, "Hello World!");
   } while( u8g.nextPage() );
 
