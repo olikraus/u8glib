9ca4233d5093fb43537b350f26b2a83ef5c31785
olikraus <devnull@localhost>
2012-01-03 23:33:25 +0100

    device list generation

--- doc/do_device_wiki.sh
+++ doc/do_device_wiki.sh
@@ -1,1 +1,4 @@
-../tools/table2html/table2html device.table > device.html
+cat device.pre > device.wiki
+../tools/table2html/table2html device.table >> device.wiki
+cat device.post >> device.wiki
+cp device.wiki ../../wiki/.
