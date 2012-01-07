da734280fa2a1e3bb5a7c159157535b8281d3392
olikraus <devnull@localhost>
2012-01-07 09:23:50 +0100

    KS0108, made rw optional

--- cppsrc/U8glib.h
+++ cppsrc/U8glib.h
@@ -198,7 +198,7 @@ class U8GLIB_KS0108_128 : public U8GLIB
 {
   public:
     U8GLIB_KS0108_128(uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7, 
-        uint8_t en, uint8_t cs1, uint8_t cs2, uint8_t di, uint8_t rw, uint8_t reset = U8G_PIN_NONE) 
+        uint8_t en, uint8_t cs1, uint8_t cs2, uint8_t di, uint8_t rw = U8G_PIN_NONE, uint8_t reset = U8G_PIN_NONE) 
       : U8GLIB(&u8g_dev_ks0108_128x64_fast, d0, d1, d2, d3, d4, d5, d6, d7, en, cs1, cs2, di, rw, reset)
       { }
 };
