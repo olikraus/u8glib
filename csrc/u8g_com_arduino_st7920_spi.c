95568ffd7f258e24dadd2fd59f5291427be4e517
olikraus <devnull@localhost>
2012-01-24 22:15:40 +0100

    beta release v0.11

--- csrc/u8g_com_arduino_st7920_spi.c
+++ csrc/u8g_com_arduino_st7920_spi.c
@@ -181,7 +181,7 @@ uint8_t u8g_com_arduino_st7920_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, 
 
 #else /* ARDUINO */
 
-uint8_t u8g_com_arduino_sw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr)
+uint8_t u8g_com_arduino_st7920_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr)
 {
   return 1;
 }
