80216bcd60492d4b005fe04c18cf62823bd4d1e1
olikraus <devnull@localhost>
2012-03-03 18:28:20 +0100

    fixes because of forgotten 16v2 device

--- sys/sdl/dev/u8g_dev_sdl.c
+++ sys/sdl/dev/u8g_dev_sdl.c
@@ -322,7 +322,8 @@ uint8_t u8g_dev_sdl_2bit_fn(u8g_t *u8g, u8g_dev_t *dev, uint8_t msg, void *arg)
 
 uint8_t u8g_dev_sdl_2bit_double_mem_fn(u8g_t *u8g, u8g_dev_t *dev, uint8_t msg, void *arg)
 {
-  
+  return 0;
+#ifdef TEMPORARY_REMOVED  
   switch(msg)
   {
     case U8G_DEV_MSG_INIT:
@@ -370,7 +371,9 @@ uint8_t u8g_dev_sdl_2bit_double_mem_fn(u8g_t *u8g, u8g_dev_t *dev, uint8_t msg, 
       SDL_UpdateRect(u8g_sdl_screen, 0,0,0,0);
       break;    /* continue to base fn */
   }
+  /* this is unsolved at the moment */
   return u8g_dev_pb16v2_base_fn(u8g, dev, msg, arg);
+#endif
 }
 
 
