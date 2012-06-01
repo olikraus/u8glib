7ff314899591b32a51a44a7beaa2f3de72e898c1
olikraus <devnull@localhost>
2012-06-01 22:09:46 +0200

    m2icon_7 update

--- fntsrc/u8g_font_m2icon_7.c
+++ fntsrc/u8g_font_m2icon_7.c
@@ -2,17 +2,17 @@
   Fontname: m2icon_7
   Copyright: public domain
   Capital A Height: 7, '1' Height: 0
-  Calculated Max Values w=12 h= 7 x= 0 y= 1 dx=13 dy= 0 ascent= 7 len=14
+  Calculated Max Values w=12 h= 8 x= 0 y= 1 dx=13 dy= 0 ascent= 7 len=14
   Font Bounding box     w=12 h= 8 x= 0 y=-1
-  Calculated Min Values           x= 0 y= 0 dx= 0 dy= 0
-  Pure Font   ascent = 7 descent= 0
-  X Font      ascent = 7 descent= 0
-  Max Font    ascent = 7 descent= 0
+  Calculated Min Values           x= 0 y=-1 dx= 0 dy= 0
+  Pure Font   ascent = 7 descent=-1
+  X Font      ascent = 7 descent=-1
+  Max Font    ascent = 7 descent=-1
 */
 #include "u8g.h"
-const u8g_fntpgm_uint8_t u8g_font_m2icon_7[274] U8G_SECTION(".progmem.u8g_font_m2icon_7") = {
-  1,12,8,0,255,7,0,17,0,214,65,104,0,7,0,7,
-  0,2,151,174,15,0,255,128,128,128,128,128,128,128,128,128,
+const u8g_fntpgm_uint8_t u8g_font_m2icon_7[275] U8G_SECTION(".progmem.u8g_font_m2icon_7") = {
+  1,12,8,0,255,7,0,17,0,214,65,104,255,7,255,7,
+  255,2,151,174,15,0,255,128,128,128,128,128,128,128,128,128,
   255,128,2,87,103,56,104,232,136,136,136,248,3,84,100,216,
   112,112,216,2,134,150,3,6,12,216,112,32,2,102,118,252,
   132,132,132,132,252,2,102,118,252,132,180,180,132,252,2,102,
@@ -26,6 +26,6 @@ const u8g_fntpgm_uint8_t u8g_font_m2icon_7[274] U8G_SECTION(".progmem.u8g_font_m
   2,102,118,120,252,252,252,252,120,255,255,255,255,255,255,255,
   255,255,255,255,255,255,2,135,151,16,48,95,129,95,48,16,
   2,119,135,16,40,68,238,40,40,56,255,255,255,2,199,222,
-  15,0,255,128,128,128,159,240,160,32,192,64,255,128,2,23,
-  39,128,128,128,128,128,128,128,2,119,199,16,0,16,186,16,
-  0,16};
+  15,0,255,128,128,128,159,240,160,32,192,64,255,128,1,24,
+  40,128,128,128,128,128,128,128,128,2,119,199,16,0,16,186,
+  16,0,16};
