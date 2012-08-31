1cc6b348ac0bf893b8ad027ab4a4456eea3ee544
olikraus <devnull@localhost>
2012-08-31 23:13:35 +0200

    nhd31

--- csrc/u8g_dev_ssd1322_nhd31oled_bw.c
+++ csrc/u8g_dev_ssd1322_nhd31oled_bw.c
@@ -367,7 +367,7 @@ static  void u8g_dev_ssd1322_1bit_write_16_pixel(u8g_t *u8g, u8g_dev_t *dev, uin
   {
     buf[0] = 0x0f0;
     buf[1] = 0x00f;
-    buf[2] = 0x000;
+    buf[2] = 0x00f;
     buf[3] = 0x000;
     buf[4] = 0x000;
     buf[5] = 0x000;
