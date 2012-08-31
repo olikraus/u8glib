3e7f6b46bb17830d0fe0d6274c6ca617d68ace88
olikraus <devnull@localhost>
2012-08-31 22:14:51 +0200

    nhd31

--- csrc/u8g_dev_ssd1322_nhd31oled_bw.c
+++ csrc/u8g_dev_ssd1322_nhd31oled_bw.c
@@ -403,7 +403,6 @@ uint8_t u8g_dev_ssd1322_nhd31oled_bw_fn(u8g_t *u8g, u8g_dev_t *dev, uint8_t msg,
       break;
     case U8G_DEV_MSG_PAGE_NEXT:
       {
-	// disabled for testing
         u8g_dev_ssd1322_1bit_prepare_page(u8g, dev);
         u8g_dev_ssd1322_1bit_write_buffer(u8g, dev, 0);
         u8g_SetChipSelect(u8g, dev, 0);        
