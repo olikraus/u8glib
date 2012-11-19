feec51add9b7baeb3f8147f7fc28ce9f053293cc
olikraus <devnull@localhost>
2012-11-19 22:06:45 +0100

    fixed adc procedure

--- sys/attiny84/analog_value/analog_value.c
+++ sys/attiny84/analog_value/analog_value.c
@@ -91,31 +91,33 @@ void sys_init(void)
 uint16_t sys_diff_adc_2_3(uint8_t gain_bit)
 {
   uint16_t l, h;
+  
+  /* datasheet recomends to turn off ADC for differntial measurement first */
+  ADCSRA = 0x00 | ADC_PRESCALAR;		/* turn off ADC */  
 
   /* use PA2 and PA3 as input  */
   DDRA &= ~_BV(2);
   DDRA &= ~_BV(3);
 
-  /* enable, but do not start ADC, clear interrupt flag */
+  /* enable, but do not start ADC (ADEN, Bit 7) */
+  /* clear the interrupt indicator flag (ADIF, Bit 4) */
   ADCSRA = 0x90 | ADC_PRESCALAR;
   
-  /* select differential input at ADC2 and ADC3, wait for conversion finished */
-  while( ADCSRA & _BV(6) )
-    ;
   ADMUX = 16 | gain_bit;
   /* enable bipolar mode, voltage diff may be higher or lower, result is signed */
   ADCSRB = 0x080;
-  /* enable and start conversion, maximum prescalar */
+  /* enable and start conversion  */
   ADCSRA = 0xc0|ADC_PRESCALAR;
-  /* wait for conversion to be finished */
-  //while ( ADCSRA == 0xc7 )
-  //  ;
-  /* wait for ADIF (Bit 4) */
+  /* wait for conversion to be finished (ADIF, Bit 4) */
   while ( (ADCSRA & _BV(4)) == 0 )
     ;
   /* return 8 bit result */
   l = ADCL;
   h = ADCH;
+  
+  /* save some power */
+  ADCSRA = 0x00 | ADC_PRESCALAR;		/* turn off ADC */  
+  
   return (h<<8) | l ;  
 }
 
@@ -125,30 +127,35 @@ uint16_t sys_adc7(void)
 {
   uint16_t l, h;
 
+  /* turn off ADC to force long conversion */
+  ADCSRA = 0x00 | ADC_PRESCALAR;		/* turn off ADC */  
+
   /* use PA7 as input pin for the ADC */
   DDRA &= ~_BV(7);
   
-  /* enable, but do not start ADC, clear interrupt flag */
+  /* enable, but do not start ADC (ADEN, Bit 7) */
+  /* clear the interrupt indicator flag (ADIF, Bit 4) */
   ADCSRA = 0x90 | ADC_PRESCALAR;
   
   /* ADC 7, wait for conversion finished*/
-  while( ADCSRA & _BV(6) )
-    ;
+  //while( ADCSRA & _BV(6) )
+  //  ;
   ADMUX = 7;	
   /* default operation */
   ADCSRB = 0x0;
   /* enable and start conversion, maximum prescalar */
   ADCSRA = 0xc0|ADC_PRESCALAR;
-  /* wait for conversion to be finished */
-  //while ( ADCSRA == 0xc7 )
-  //  ;
-  /* wait for ADIF (Bit 4) */
+  /* wait for conversion to be finished (ADIF, Bit 4) */
   while ( (ADCSRA & _BV(4)) == 0 )
     ;
   /* return 8 bit result */
   
   l = ADCL;
   h = ADCH;
+
+  /* save some power */
+  ADCSRA = 0x00 | ADC_PRESCALAR;		/* turn off ADC */  
+  
   return (h<<8) | l ;
 }
 
@@ -168,22 +175,45 @@ uint16_t adc_val1 = 0;
 uint16_t adc_val2 = 0;
 uint8_t sign;
 uint16_t val;
+int16_t sval;
+int16_t min = 2000;
+int16_t max = -2000;
+
+
+void draw_signed(uint8_t x, uint8_t y, int16_t v)
+{
+  if ( v < 0 )
+  {
+    u8g_DrawStr(&u8g, x, y, "-");
+    u8g_DrawStr(&u8g, x+8, y, u8g_u16toa(-v, 3));
+  }
+  else
+  {
+    u8g_DrawStr(&u8g, x, y, "+");
+    u8g_DrawStr(&u8g, x+8, y, u8g_u16toa(v, 3));
+  }
+}
 
 
 void draw(void)
 {
-  //u8g_SetFont(&u8g, u8g_font_fixed_v0r);
-  u8g_SetFont(&u8g, u8g_font_7x13r);
-  u8g_DrawStr(&u8g, 0, 12, "Analog Values");
-  u8g_DrawStr(&u8g, 0, 24, u8g_u16toa(adc_val1, 4));
-  u8g_DrawStr(&u8g, 40, 24, u8g_u16toa(adc_val2, 4));
-  u8g_DrawStr(&u8g, 80, 24, sign == 0 ? "+" : "-");
-  u8g_DrawStr(&u8g, 80+8, 24, u8g_u16toa(val, 3));
+  u8g_SetFont(&u8g, u8g_font_fixed_v0r);
+  //u8g_SetFont(&u8g, u8g_font_7x13r);
+  u8g_DrawStr(&u8g, 0, 10, "Analog Values");
+  u8g_DrawStr(&u8g, 0, 20, u8g_u16toa(adc_val1, 4));
+  u8g_DrawStr(&u8g, 40, 20, u8g_u16toa(adc_val2, 4));
+  u8g_DrawStr(&u8g, 80, 20, sign == 0 ? "+" : "-");
+  u8g_DrawStr(&u8g, 80+8, 20, u8g_u16toa(val, 3));
+  
+  draw_signed(0, 30, sval);
+  draw_signed(40, 30, min);
+  draw_signed(80, 30, max);
   
 }
 
 int main(void)
 {
+  uint8_t i;
   sys_init();
   u8g_setup();
 
@@ -193,19 +223,32 @@ int main(void)
     /* 
       why do we need two conversons???  seen notes above
     */
-    adc_val1 = sys_adc7();
-    adc_val1 = sys_adc7();
-    adc_val2 = sys_diff_adc_2_3(1);
-    adc_val2 = sys_diff_adc_2_3(1);
     
-    sign = 0;
-    val = adc_val2;
-    if ( val >= 512 )
+    for( i = 0; i < 100; i++ )
     {
-      sign = 1;
-      val = 1024-val ;
+      adc_val1 = sys_adc7();
+      //adc_val1 = sys_adc7();
+      adc_val2 = sys_diff_adc_2_3(1);
+      //adc_val2 = sys_diff_adc_2_3(1);
+      
+      sign = 0;
+      val = adc_val2;
+      if ( val >= 512 )
+      {
+	sign = 1;
+	val = 1024-val ;
+      }
+      
+      sval = val;
+      if ( sign != 0 )
+	sval = -sval;
+      
+      if ( min > sval )
+	min = sval;
+      
+      if ( max < sval )
+	max = sval;
     }
-    
     
     
     u8g_FirstPage(&u8g);
