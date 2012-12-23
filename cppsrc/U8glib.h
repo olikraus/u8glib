f0adab5df2f1765db0f2a8a1dd16afab80c39a68
olikraus <devnull@localhost>
2012-12-23 09:37:49 +0100

    tls8204

--- cppsrc/U8glib.h
+++ cppsrc/U8glib.h
@@ -607,6 +607,14 @@ class U8GLIB_PCD8544 : public U8GLIB
       { }
 };
 
+class U8GLIB_TLS8204_84X48 : public U8GLIB 
+{
+  public:
+    U8GLIB_TLS8204_84X48(uint8_t sck, uint8_t mosi, uint8_t cs, uint8_t a0, uint8_t reset = U8G_PIN_NONE) 
+      : U8GLIB(&u8g_dev_tls8204_84x48_sw_spi, sck, mosi, cs, a0, reset)
+      { }
+};
+
 class U8GLIB_KS0108_128 : public U8GLIB 
 {
   public:
--- csrc/u8g.h
+++ csrc/u8g.h
@@ -240,6 +240,7 @@ extern u8g_dev_t u8g_dev_ks0108_128x64_fast;    /* faster, but uses private tabl
 
 /* Nokia 84x48 Display with PCD8544 */
 extern u8g_dev_t u8g_dev_pcd8544_84x48_sw_spi;
+extern u8g_dev_t u8g_dev_tls8204_84x48_sw_spi;
 
 /* Nokia 96x65 Display with PCF8812 */
 extern u8g_dev_t u8g_dev_pcf8812_96x65_sw_spi;
