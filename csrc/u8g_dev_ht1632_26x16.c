ecd3a19b0cbc2fad5ea23f1ed2a04bb053b1e1d7
olikraus <devnull@localhost>
2013-05-08 23:26:33 +0200

    fixed year

--- csrc/u8g_dev_ht1632_26x16.c
+++ csrc/u8g_dev_ht1632_26x16.c
@@ -6,7 +6,7 @@
 
   Universal 8bit Graphics Library
   
-  Copyright (c) 2011, olikraus@gmail.com
+  Copyright (c) 2013, olikraus@gmail.com
   All rights reserved.
 
   Redistribution and use in source and binary forms, with or without modification, 
