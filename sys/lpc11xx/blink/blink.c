3a5b61537fb1e5b2306743e0234c1d1a8f85916f
olikraus <devnull@localhost>
2013-06-08 10:12:58 +0200

    sys tick example

--- sys/lpc11xx/blink/blink.c
+++ sys/lpc11xx/blink/blink.c
@@ -3,14 +3,15 @@
 
 uint32_t SystemCoreClock;
 
+
 void SystemInit()
 {
   /* SystemInit() is called by the startup code */
   /* according to system_LPC11xx.h it is expected, that the clock freq is set here */
-  SystemCoreClock = F_CPU;
+  SystemCoreClock = F_CPU;  
 }
-  
-  
+
+
 #define LED_GPIO	LPC_GPIO1
 #define LED_PIN 8			
 
@@ -19,7 +20,9 @@ void main()
 {
 	volatile uint32_t count, count_max = 100000;
 	LED_GPIO->DIR |= 1 << LED_PIN;		
+	LED_GPIO->DATA |= 1 << LED_PIN;
 
+  
 	while (1)
 	{
 		for (count = 0; count < count_max; count++);	// delay
---
+++ sys/lpc11xx/systickblink/systickblink.c
@@ -0,0 +1,42 @@
+
+#include "LPC11xx.h"
+
+uint32_t SystemCoreClock;
+
+#define SYS_TICK_PERIOD_IN_MS 100
+
+void SystemInit()
+{
+  /* SystemInit() is called by the startup code */
+  /* according to system_LPC11xx.h it is expected, that the clock freq is set here */
+  SystemCoreClock = F_CPU;
+  
+  /* SysTick is defined in core_cm0.h */
+  SysTick->CTRL = 7;   /* enable, generate interrupt, do not divide by 2 */
+  SysTick->LOAD = (SystemCoreClock/1000UL*(unsigned long)SYS_TICK_PERIOD_IN_MS) - 1;
+}
+
+#define LED_GPIO	LPC_GPIO1
+#define LED_PIN 8			
+
+
+volatile uint32_t sys_tick_irq_cnt=0;
+
+void __attribute__ ((interrupt)) SysTick_Handler(void)
+{
+  sys_tick_irq_cnt++;
+  LED_GPIO->DIR |= 1 << LED_PIN;	
+  if ( sys_tick_irq_cnt & 1 )
+    LED_GPIO->DATA |= 1 << LED_PIN;
+  else
+    LED_GPIO->DATA &= ~(1 << LED_PIN);
+}
+  
+  
+
+void main()
+{
+	while (1)
+	{
+	}
+}
