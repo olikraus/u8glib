f85ebafe5566f01d40e7ce7ac289392996bdee62
olikraus <devnull@localhost>
2013-06-09 11:45:42 +0200

    arm delay blink

--- sys/lpc11xx/delayblink/delayblink.c
+++ sys/lpc11xx/delayblink/delayblink.c
@@ -3,7 +3,8 @@
 
 uint32_t SystemCoreClock;
 
-#define SYS_TICK_PERIOD_IN_MS 100
+
+#define SYS_TICK_PERIOD_IN_MS 10
 
 void SystemInit()
 {
@@ -12,8 +13,8 @@ void SystemInit()
   SystemCoreClock = F_CPU;
   
   /* SysTick is defined in core_cm0.h */
-  SysTick->CTRL = 7;   /* enable, generate interrupt, do not divide by 2 */
   SysTick->LOAD = (SystemCoreClock/1000UL*(unsigned long)SYS_TICK_PERIOD_IN_MS) - 1;
+  SysTick->CTRL = 7;   /* enable, generate interrupt, do not divide by 2 */
 }
 
 #define LED_GPIO	LPC_GPIO1
@@ -25,73 +26,107 @@ volatile uint32_t sys_tick_irq_cnt=0;
 void __attribute__ ((interrupt)) SysTick_Handler(void)
 {
   sys_tick_irq_cnt++;
-  LED_GPIO->DIR |= 1 << LED_PIN;	
-  if ( sys_tick_irq_cnt & 1 )
-    LED_GPIO->DATA |= 1 << LED_PIN;
-  else
-    LED_GPIO->DATA &= ~(1 << LED_PIN);
 }
 
-static void _delay_system_ticks(uint32_t sys_ticks)
+/*
+  Delay by the provided number of system ticks.
+  The delay must be smaller than the RELOAD value.
+  This delay has an imprecision of about +/- 20 system ticks.   
+*/
+static void _delay_system_ticks_sub(uint32_t sys_ticks)
 {
-  uint32_t start_val, end_val;
+  uint32_t start_val, end_val, curr_val;
+  uint32_t load;
   
   start_val = SysTick->VAL;
+  start_val &= 0x0ffffffUL;
   end_val = start_val;
-  end_val += sys_ticks;
-  if ( end_val >= SysTick->LOAD )
+  
+  if ( end_val < sys_ticks )
   {
-    end_val -= SysTick->LOAD;
+    /* check, if the operation after this if clause would lead to a negative result */
+    /* if this would be the case, then add the reload value first */
+    load = SysTick->LOAD;
+    load &= 0x0ffffffUL;
+    end_val += load;
   }
+  /* counter goes towards zero, so end_val is below start value */
+  end_val -= sys_ticks;		
+  
   
   /* wait until interval is left */
-  if ( start_val < end_val )
+  if ( start_val >= end_val )		/* if there is now overflow, then this is true */
   {
-    while( start_val < SysTick->VAL && end_val > SysTick->VAL )
-      ;
+    for(;;)
+    {
+      curr_val = SysTick->VAL;
+      curr_val &= 0x0ffffffUL;
+      if ( curr_val <= end_val )
+	break;
+      if ( curr_val > start_val )
+	break;
+    }
   }
   else
   {
-    while( start_val < SysTick->VAL || end_val > SysTick->VAL )
-      ;
+    for(;;)
+    {
+      curr_val = SysTick->VAL;
+      curr_val &= 0x0ffffffUL;
+      if ( curr_val <= end_val && curr_val > start_val )
+	break;
+    }
   }
 }
 
-static void delay_system_ticks(uint32_t sys_ticks)
+/*
+  Delay by the provided number of system ticks.
+  Any values between 0 and 0x0ffffffff are allowed.
+*/
+void delay_system_ticks(uint32_t sys_ticks)
 {
-  uint32_t half_load = SysTick->LOAD / 2;
-  while ( sys_ticks > half_load )
+  uint32_t load4;
+  load4 = SysTick->LOAD;
+  load4 &= 0x0ffffffUL;
+  load4 >>= 2;
+  
+  while ( sys_ticks > load4 )
   {
-    sys_ticks -= half_load;
-    _delay_system_ticks(half_load);
+    sys_ticks -= load4;
+    _delay_system_ticks_sub(load4);
   }
-  _delay_system_ticks(sys_ticks);
+  _delay_system_ticks_sub(sys_ticks);
 }
-
 
 /*
   Delay by the provided number of micro seconds.
+  Limitation: "us" * System-Freq in MHz must now overflow in 32 bit.
+  Values between 0 and 1.000.000 (1 second) are ok.
 */
 void delay_micro_seconds(uint32_t us)
 {
   uint32_t sys_ticks;
-  uint32_t start_val, end_val;
 
 #ifdef F_CPU 
-  sys_ticks = F_CPU / 10000000UL * us;
+  sys_ticks = F_CPU / 1000000UL * us;
 #else  
   sys_ticks = SystemCoreClock;
-  sys_ticks /=10000000UL;
+  sys_ticks /=1000000UL;
   sys_ticks *= us;
 #endif
   delay_system_ticks(sys_ticks);  
 }
   
-
 void main()
 {
-	while (1)
-	{
-	  delay_micro_seconds(100);
-	}
+  uint32_t us = 100000;		/* 0.1 s */
+  LED_GPIO->DIR |= 1 << LED_PIN;	  
+  
+  while (1)
+  {
+    delay_micro_seconds(us);
+    LED_GPIO->DATA |= 1 << LED_PIN;
+    delay_micro_seconds(us);
+    LED_GPIO->DATA &= ~(1 << LED_PIN);
+  }
 }
