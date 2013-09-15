e3e7c7ec0b6cb07a0f73bd181dfd7975060a9029
olikraus <devnull@localhost>
2013-09-15 11:03:50 +0200

    u8g_logo

--- sys/arm/examples/u8g_logo/u8g_logo.c
+++ sys/arm/examples/u8g_logo/u8g_logo.c
@@ -1,9 +1,9 @@
 /*
   
-  hello_world.c
-  
-  >>> PLEASE UNCOMMENT DISPLAY TYPE IN THE MAIN PROCEDURE <<<
-  
+  u8g_logo.c
+
+  >>> PLEASE UNCOMMENT DISPLAY TYPE IN THE MAIN PROCEDURE <<<  
+
   ARM U8glib Example
 
   Universal 8bit Graphics Library
@@ -64,37 +64,70 @@ void __attribute__ ((interrupt)) SysTick_Handler(void)
 
 u8g_t u8g;
 
-void draw(uint8_t pos)
+void draw_color_box(void)
 {
-  if ( u8g_GetMode(&u8g) == U8G_MODE_HICOLOR || u8g_GetMode(&u8g) == U8G_MODE_R3G3B2) {
-    /* draw background (area is 128x128) */
-    u8g_uint_t r, g, b;
-    for( b = 0; b < 4; b++ )
-    {
-      for( g = 0; g < 32; g++ )
+  u8g_uint_t w,h;
+  u8g_uint_t r, g, b;
+  
+  w = u8g_GetWidth(&u8g)/32;
+  h = u8g_GetHeight(&u8g)/8;
+  for( b = 0; b < 4; b++ )
+    for( g = 0; g < 8; g++ )
+      for( r = 0; r < 8; r++ )
       {
-	for( r = 0; r < 32; r++ )
-	{
-	  u8g_SetRGB(&u8g, r<<3, g<<3, b<<4 );
-	  u8g_DrawPixel(&u8g, g + b*32, r);
-	  u8g_SetRGB(&u8g, r<<3, g<<3, (b<<4)+64 );
-	  u8g_DrawPixel(&u8g, g + b*32, r+32);
-	  u8g_SetRGB(&u8g, r<<3, g<<3, (b<<4)+128 );
-	  u8g_DrawPixel(&u8g, g + b*32, r+32+32);
-	  u8g_SetRGB(&u8g, r<<3, g<<3, (b<<4)+128+64 );
-	  u8g_DrawPixel(&u8g, g + b*32, r+32+32+32);
-	}
+        u8g_SetColorIndex(&u8g, (r<<5) |  (g<<2) | b );
+        u8g_DrawBox(&u8g, g*w + b*w*8, r*h, w, h);
       }
-    }
-    u8g_SetRGB(&u8g, 255,255,255);
+}
+
+void draw_logo(uint8_t d)
+{
+  u8g_SetFont(&u8g, u8g_font_gdr25r);
+  u8g_DrawStr(&u8g, 0+d, 30+d, "U");
+  u8g_SetFont(&u8g, u8g_font_gdr30n);
+  u8g_DrawStr90(&u8g, 23+d,10+d,"8");
+  u8g_SetFont(&u8g, u8g_font_gdr25r);
+  u8g_DrawStr(&u8g, 53+d,30+d,"g");
+  
+  u8g_DrawHLine(&u8g, 2+d, 35+d, 47);
+  u8g_DrawVLine(&u8g, 45+d, 32+d, 12);
+}
+
+void draw_url(void)
+{
+  u8g_SetFont(&u8g, u8g_font_4x6);
+  if ( u8g_GetHeight(&u8g) < 59 )
+  {
+    u8g_DrawStr(&u8g, 53,9,"code.google.com");
+    u8g_DrawStr(&u8g, 77,18,"/p/u8glib");
   }
-  u8g_SetFont(&u8g, u8g_font_unifont);
-  u8g_DrawStr(&u8g,  0, 12+pos, "Hello World!");
+  else
+  {
+    u8g_DrawStr(&u8g, 1,54,"code.google.com/p/u8glib");
+  }
+}
+
+
+void draw(void) 
+{
+  if ( u8g_GetMode(&u8g) == U8G_MODE_HICOLOR || u8g_GetMode(&u8g) == U8G_MODE_R3G3B2)
+  {
+    draw_color_box();
+  }
+  u8g_SetColorIndex(&u8g, 1);
+  if ( U8G_MODE_GET_BITS_PER_PIXEL(u8g_GetMode(&u8g)) > 1 ) 
+  {
+    draw_logo(2);
+    u8g_SetColorIndex(&u8g, 2);
+    draw_logo(1);
+    u8g_SetColorIndex(&u8g, 3);
+  }
+  draw_logo(0);
+  draw_url();
 }
 
 void main()
 {
-  uint8_t pos = 0;
   /*
     Please uncomment one of the displays below
     Notes:
@@ -155,15 +188,12 @@ void main()
     u8g_FirstPage(&u8g);
     do
     {
-      draw(pos);
+      draw();
     } while ( u8g_NextPage(&u8g) );
     
     /* refresh screen after some delay */
     u8g_Delay(100);
     
-    /* update position */
-    pos++;
-    pos &= 15;
   }  
 }
 
