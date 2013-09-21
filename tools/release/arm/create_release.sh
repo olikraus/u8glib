5d00991e72c76d47047670a6dba38c801d2a3ecc
olikraus <devnull@localhost>
2013-09-21 21:48:15 +0200

    arm release

--- tools/release/arm/create_release.sh
+++ tools/release/arm/create_release.sh
@@ -1,8 +1,18 @@
-pushd .
+# pushd .
+# cd ..
+# cd ..
+# cd ..
+# cd sys
+# cd arm
+# ind -type d -exec sh -c "cd {} && make clean" \;
+# popd
+rm -rf u8g_arm
+mkdir u8g_arm
+cd u8g_arm
+mkdir src
+cd src
+cp ../../../../../csrc/*.[hc] .
+cp ../../../../../sfntsrc/*.[hc] .
 cd ..
 cd ..
-cd ..
-cd sys
-cd arm
-find -type d -exec sh -c "cd {} && make clean" \;
-popd
+./sub_create.sh	lpc1114
